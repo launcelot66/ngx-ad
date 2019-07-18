d41241e531561c4a1db17f9970bcac3b311180e4
lans <lanselot96@gmail.com>
2019-07-18 18:02:53 +0500

    init AutoDealer Angular Table Library

---
+++ projects/table/src/lib/table.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ngx-ad-table',
+  templateUrl: './table.component.html',
+  styleUrls: [ './table.component.scss' ]
+})
+export class TableComponent {
+
+  constructor() { }
+
+}
