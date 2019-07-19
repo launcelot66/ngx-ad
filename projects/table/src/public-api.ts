4320c65fa06509b9d06abd02dad824d934a3d5b3
lans <lanselot96@gmail.com>
2019-07-19 18:04:18 +0500

    little fix

--- projects/table/src/public-api.ts
+++ projects/table/src/public-api.ts
@@ -5,3 +5,10 @@
 export * from './lib/table/table.service';
 export * from './lib/table/table.component';
 export * from './lib/table/table.module';
+export * from './lib/row/row.service';
+export * from './lib/row/row.component';
+export * from './lib/row/row.module';
+export * from './lib/cells/one-field/one-field.service';
+export * from './lib/cells/one-field/one-field.component';
+export * from './lib/cells/one-field/one-field.module';
+
